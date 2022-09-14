d4c5e9fbf15aacf918ff16386bad7d066c55925f
Polina <112948671+apolinarys@users.noreply.github.com>
2022-09-14 15:45:12 +0300

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Macbook on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
